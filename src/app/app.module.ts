1c8620056230bbc85a9c3b6a77e2bd2fef6f7a45
Mohibur-CoU-ICT <mohiburrahman5509@gmail.com>
2022-12-11 22:25:17 +0600

    time.is moved to time component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TimeComponent } from './time/time.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimeComponent
   ],
   imports: [
     BrowserModule,
